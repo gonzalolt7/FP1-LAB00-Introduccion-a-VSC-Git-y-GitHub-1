79e2c7b4c1b6e0b55e8966fe9268a7ac9889c6f8
Gonzalo <gonloptor1@alum.us.es>
2025-09-19 16:29:45 +0200

    Nuevo archivo saludo

---
+++ src/saludo.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+hora_actual = datetime.now().hour
+if hora_actual < 12:
+    print("¡Buenos días pepe!")
+elif 12 < hora_actual <22:
+    print("¡Buenas tardes pepe!")
+else: print("¡Buenas noches pepe!")
